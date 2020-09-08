3150715cb60173cc0e48998b333a6c126e97a23b
Wasiu Idowu <hoxtygen@live.com>
2020-09-08 08:42:23 +0100

    Add character class

---
+++ lib/character.dart
@@ -0,0 +1,47 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Character {
+  final String name;
+  final String gender;
+  final String height;
+  final String mass;
+  final String hairColor;
+  final String skinColor;
+  final String homeWorld;
+
+  Character(
+      {this.name,
+      this.gender,
+      this.height,
+      this.mass,
+      this.hairColor,
+      this.skinColor,
+      this.homeWorld});
+
+  factory Character.fromJson(Map<String, dynamic> json) {
+    return Character(
+      name: json['name'],
+      gender: json['gender'],
+      height: json['height'],
+      mass: json['mass'],
+      hairColor: json['hairColor'],
+      skinColor: json['skinColor'],
+      homeWorld: json['homeWorld'],
+    );
+  }
+
+  Future<Character> fetchAlbum() async {
+    final response = await http.get('https://swapi.dev/api/people/');
+
+    if (response.statusCode == 200) {
+      // print(response.body);
+      // print(json.decode(response.body));
+      return Character.fromJson(json.decode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+}
